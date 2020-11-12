69567119d7df98e038d5e79b74b18f2783fc2ed1
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-11-12 21:43:09 +0000

    for loop exercise: repeat chorus

---
+++ chorus-for.js
@@ -0,0 +1,8 @@
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 3; repeat++) {
+  if (repeat === 1){
+    console.log('*change tone*')
+  } 
+  console.log(chorus);
+}
+console.log("Until the sun comes up!"); 
