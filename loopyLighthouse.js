045d93d2b79f1901f3a46a903c5ebcb9e9a0a112
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-11-12 21:56:08 +0000

    find exceptions using remainder operator in a loop

---
+++ loopyLighthouse.js
@@ -0,0 +1,6 @@
+var x = 100;
+
+while (x <= 200) {
+    (x%3 === 0) ? ((x%4 === 0) ? console.log("LoopyLighthouse") : console.log("Loopy")) : ((x%4 === 0) ? console.log("Lighthouse") : console.log(x));
+    x++;
+}
