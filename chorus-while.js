95ae6e8c810a723415feaec751db5e4535d3cb52
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-11-12 21:39:29 +0000

    while loop exercise: repeat chorus

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 3) {
+  if (repeat === 1){
+    console.log('*change key*')
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
