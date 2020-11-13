4d6efc301f180d33ef042548bbdb1a554670b1d3
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-11-13 05:15:08 +0000

    area calculator functions for rectangles, triangles, and circles

---
+++ area-calculators.js
@@ -0,0 +1,20 @@
+function calculateRectangleArea(length, width){
+  if(length >= 0 && width >= 0){
+    return length * width;
+  }
+  return undefined;
+}
+
+function calculateTriangleArea(base, height){
+  if(base >= 0 && height >= 0){
+    return (base * height) / 2;
+  }
+  return undefined
+}
+
+function calculateCircleArea(radius){
+  if(radius >= 0){
+    return Math.PI * Math.pow(radius, 2);
+  }
+  return undefined
+}
